06ce351f5625effd85b1faf8bd5862fe57cf456c
Holden Karau <holden@pigscanfly.ca>
2010-10-03 01:17:31 -0700

    Add find self

--- bottoms/perl/runme.pl
+++ bottoms/perl/runme.pl
@@ -4,7 +4,26 @@ use warnings;
 
 while (1) {
     my ($width,@tiles) = read_board();
+    my @self = find_self($width,@tiles);
     do_move('L');
+}
+
+sub find_self {
+    my ($width,@tiles) = @_;
+    for (my $i = 0 ; $i <= $#tiles; ++$i) {
+	if ($tiles[$i] eq 'C' || $tiles[$i] eq '@') {
+	    my @pos = (int($i/$width),$i%$width);
+	    return @pos;
+	}
+    }
+    return -1;#Should not happen
+}
+
+sub is_valid  {
+    my $width = shift @_;
+    my $x = shift @_;
+    my $y = shift @_;
+    return (0 <= $x && 0 <= $y && $x < $width && $y < $width);
 }
 
 sub do_move {
