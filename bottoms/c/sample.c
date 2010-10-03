ecd7ac5ffc4270f7bf2983fe4468157e40a23a08
Michael Gregson <mgregson@csclub.uwaterloo.ca>
2010-10-03 03:36:35 -0700

    Use correct bounds-checking in sample code.

--- bottoms/c/sample.c
+++ bottoms/c/sample.c
@@ -6,15 +6,18 @@ int main(int argc, char** argv)
   init();
   while(read_world() > 0)
     {
-      if(pos_x >= 1 && CELL(pos_x-1,pos_y) != WALL)
+      if(IN_BOUNDS(pos_x-1) && IN_BOUNDS(pos_y)
+	 && CELL(pos_x-1,pos_y) != WALL)
 	{
 	  left();
 	}
-      else if(pos_y >= 1 && CELL(pos_x,pos_y-1) != WALL)
+      else if(IN_BOUNDS(pos_y-1) && IN_BOUNDS(pos_x)
+	      && CELL(pos_x,pos_y-1) != WALL)
 	{
 	  up();
 	}
-      else if(pos_x+1 < width && CELL(pos_x+1,pos_y) != WALL)
+      else if(IN_BOUNDS(pos_x+1) && IN_BOUNDS(pos_y)
+	      && CELL(pos_x+1,pos_y) != WALL)
 	{
 	  right();
 	}
